3c96c5b12d089bca9891b2587742aba7e04ef9b8
Vindya2004 <vindyamadubhashini@gmail.com>
2025-08-18 14:31:36 +0530

    init project

---
+++ src/main/java/org/example/awrms/AwrmsApplication.java
@@ -0,0 +1,13 @@
+package org.example.awrms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwrmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwrmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/awrms/AwrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.awrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwrmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
